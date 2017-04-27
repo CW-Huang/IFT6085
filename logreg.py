b459c2377f2d641a3a410702d9b9c41c840683e7
Shawn Tan <shawn@wtf.sg>
2017-04-28 03:23:35 +0800

    Logreg

--- logreg.py
+++ logreg.py
@@ -4,6 +4,8 @@ import theano
 import theano.tensor as T
 import numpy as np
 import lasagne
+import math
+import random
 
 def load_mnist(path):
 
@@ -22,17 +24,77 @@ def load_mnist(path):
 
     return train_x, train_y, valid_x, valid_y, test_x, test_y
 
+update_fun = {
+    "adam": lasagne.updates.adam,
+    "sgd": lasagne.updates.sgd,
+    "momentum": lasagne.updates.momentum,
+}
 
 if __name__ == "__main__":
     path = r'/data/lisa/data/mnist/mnist.pkl.gz'
+    batch_size = 64
     train_x, train_y, _, _, _, _ = load_mnist(path)
+    data_x = theano.shared(train_x.astype(np.float32))
+    data_y = theano.shared(train_y.astype(np.float32))
 
     W = theano.shared(np.zeros(28 * 28, 10).astype(np.float32))
     b = theano.shared(np.zeros(10).astype(np.float32))
     params = [W, b]
+
     X = T.matrix('X')
     y = T.ivector('y')
     y_hat = T.nnet.softmax(T.dot(X, W) + b)
     cost = T.mean(T.nnet.categorical_crossentropy(y_hat, y))
     gradients = T.grad(cost, wrt=params)
-    updates = lasagne.updates.adam(gradients, params, lr)
+
+    update_type = "adam"
+    updates = update_fun[update_type](gradients, params, lr)
+    deltas = [updates[p] - p for p in params]
+
+    idx = T.iscalar('idx')
+    train = theano.function(
+        inputs=[idx],
+        outputs=cost,
+        givens={
+            X:data_x[idx * batch_size:(idx + 1) * batch_size],
+            y:data_y[idx * batch_size:(idx + 1) * batch_size],
+        }
+    )
+
+    per_instance_gradient = theano.function(
+        inputs=[idx],
+        outputs=gradients,
+        givens={
+            X:data_x[idx:(idx + 1)],
+            y:data_y[idx:(idx + 1)],
+        }
+    )
+
+    def calculate_variance():
+        acc_grad = None
+        acc_grad_sqr = None
+        count = train_x.shape[0]
+        for i in xrange(count):
+            grads = per_instance_gradient(i)
+            if acc_grad is None:
+                acc_grad = [np.zeros(g.shape).astype(np.float32)
+                            for g in grads]
+                acc_grad_sqr = [np.zeros(g.shape).astype(np.float32)
+                                for g in grads]
+            for ag, ags, g in zip(acc_grad, acc_grad_sqr, grads):
+                ag += g
+                ags += g**2
+        variances = [ags / count - (ag / count)**2
+                     for ag, ags in zip(acc_grad, acc_grad_sqr)]
+        return variances
+
+    batches = math.ceil(train_x.shape[0] / float(batch_size))
+    iterations = 0
+    for epoch in xrange(epochs):
+        batch_idxs = range(batches)
+        random.shuffle(batch_idxs)
+        for i in batch_idxs:
+            print train(i)
+            iterations += 1
+            if iterations % 100 = 0:
+                variances = calculate_variance()
