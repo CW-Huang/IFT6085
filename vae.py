Correcting some bugs. Now we can monitor the variance of the update for both VAE and SVRG

vae.py
@@ -142,22 +142,6 @@ class VAE(object):
 
         self.grads = T.grad(self.loss, self.params)
 
-        # -- begin --
-        # hacky hack for getting instance gradients in a batch
-        # batch_size = T.cast(self.inpv.shape[0], 'float32')
-        # for g in self.grads:
-        #     if len(g.owner.inputs) == 2:
-        #         layer_input = g.owner.inputs[0].dimshuffle(1, 0, 'x')
-        #         delta_output = g.owner.inputs[1].dimshuffle(0, 'x', 1)
-        #         instance_g = batch_size * layer_input * delta_output
-        #     elif len(g.owner.inputs) == 1:
-        #         instance_g = batch_size * (
-        #             g.owner.inputs[0].owner.inputs[0].owner.inputs[0].owner.inputs[0].owner)
-        #     else:
-        #         print g.owner
-        # Theano is our _____
-        # -- end --
-
         self.mgrads = lasagne.updates.total_norm_constraint(self.grads,
                                                             max_norm=max_norm)
         self.cgrads = [T.clip(g, -clip_grad, clip_grad) for g in self.mgrads]
@@ -182,56 +166,160 @@ class VAE(object):
         #self.get_mu = theano.function([self.inpv],self.mu)
         #self.get_var = theano.function([self.inpv],self.var)
 
-        # var stuff
-        batch_size = T.cast(self.inpv.shape[0], 'float32')
-        self.grads_accumulate = [
-            theano.shared(np.zeros(p.get_value().shape).astype(np.float32))
-            for p in self.params
-        ]
+        self.monitor_updates_variance()
 
-        # for the variance
-        self.grads2_accumulate = [
-            theano.shared(np.zeros(p.get_value().shape).astype(np.float32))
-            for p in self.params
+
+    def tract_covariance(self, variable1, variable2, shapes, inc, inputs):
+
+        """
+        Return the covariance of variable 1 and 2, and the variance of v1 and v2.
+         If only variable 1 is present, only return var(v1).
+        :param variable1:
+        :param variable2:
+        :param shapes:
+        :param inc:
+        :param inputs:
+        :return:
+        """
+
+        # all of our moments
+        accumulate_m1 = [
+            theano.shared(np.zeros(p).astype(np.float32))
+            for p in shapes
             ]
 
-        # for the mean
-        acc_grads_update = [
-            (a, a + batch_size * g)
-            for a, g in zip(self.grads_accumulate, self.cgrads)
-        ]
+        accumulate_m11 = [
+            theano.shared(np.zeros(p).astype(np.float32))
+            for p in shapes
+            ]
+
+        accumulate_m12 = []
+        accumulate_m22 = []
+        accumulate_m2 = []
+
+        if variable2 is not None:
+            accumulate_m12 = [
+            theano.shared(np.zeros(p).astype(np.float32))
+            for p in shapes
+            ]
+
+
+            accumulate_m22 = [
+                theano.shared(np.zeros(p).astype(np.float32))
+                for p in shapes
+                ]
+
+
+            accumulate_m2 = [
+                theano.shared(np.zeros(p).astype(np.float32))
+                for p in shapes
+                ]
+
+        counter = theano.shared(np.float32(0.))
+        count_update = {counter: counter + inc}
+        reset_count_update = [(
+            counter, np.float32(0.)
+        )]
 
         # var ** 2
         # We assume that the minibatch size is one.
-        acc_grads2_update = {c: c for c in self.grads2_accumulate}
-        for ex in [self.cgrads]:
-            for cummul, grad in zip(self.grads2_accumulate, ex):
-                acc_grads2_update[cummul] = acc_grads2_update[cummul] + grad ** 2
+        # Our moment updates
 
-        reset_grads2_update = [
+        def first_moment(acc_m, v):
+
+            if v is None:
+                return {}
+
+            acc_m_update = {c: c for c in acc_m}
+            for ex in [v]:
+                for cummul, grad in zip(acc_m, ex):
+                    acc_m_update[cummul] = acc_m_update[cummul] + grad
+            return acc_m_update
+
+        def second_moment(acc_m, v1, v2):
+
+            if v1 is None or v2 is None:
+                return {}
+
+            acc_m_update = {c: c for c in acc_m}
+            for cummul, grad1, grad2 in zip(acc_m, v1, v2):
+                acc_m_update[cummul] = acc_m_update[cummul] + grad1 * grad2
+            return acc_m_update
+
+        acc_m1_update = first_moment(accumulate_m1, variable1)
+        acc_m2_update = first_moment(accumulate_m2, variable2)
+        acc_m11_update = second_moment(accumulate_m11, variable1, variable1)
+        acc_m12_update = second_moment(accumulate_m12, variable1, variable2)
+        acc_m22_update = second_moment(accumulate_m22, variable2, variable2)
+
+        cov12 = [m2/counter - m1_1*m1_2 for m1_1, m1_2, m2 in zip(accumulate_m1, accumulate_m2, accumulate_m12)]
+        cov11 = [m2/counter - m1_1*m1_2 for m1_1, m1_2, m2 in zip(accumulate_m1, accumulate_m1, accumulate_m11)]
+        cov22 = [m2/counter - m1_1*m1_2 for m1_1, m1_2, m2 in zip(accumulate_m2, accumulate_m2, accumulate_m22)]
+
+        reset_all_ms_update = [
             (a, a * np.float32(0.))
-            for a in self.grads2_accumulate
+            for a in accumulate_m1 + accumulate_m2 + accumulate_m11 + accumulate_m12 + accumulate_m22
             ]
 
-        reset_grads_update = [
-            (a, a * np.float32(0.))
-            for a in self.grads_accumulate
-        ]
+        updates = {}
+        updates.update(acc_m1_update)
+        updates.update(acc_m2_update)
+        updates.update(acc_m11_update)
+        updates.update(acc_m12_update)
+        updates.update(acc_m22_update)
+        updates.update(count_update)
 
-        self.accumulate_grads2_func = theano.function(
-            inputs=[self.inpv, self.n_mc, self.n_iw, self.ep, self.w],
-            updates=acc_grads2_update
+
+        accumulate_all_ms_func = theano.function(
+            inputs=inputs,
+            updates=updates
         )
 
-        self.accumulate_gradients_func = theano.function(
-            inputs=[self.inpv, self.n_mc, self.n_iw, self.ep, self.w],
-            updates=acc_grads_update
+        reset_all = theano.function(inputs=[], updates=reset_all_ms_update + reset_count_update)
+
+        get_cov11_func = theano.function(
+            inputs=[], outputs=cov11
         )
 
-        self.reset = theano.function(
-        inputs=[], updates=( reset_grads_update +
-        reset_grads2_update)
-        )
+        get_cov22_func = None
+        get_cov12_func = None
+
+        if variable2 is not None:
+            get_cov22_func = theano.function(
+                inputs=[], outputs=cov22
+            )
+
+            get_cov12_func = theano.function(
+                inputs=[], outputs=cov12
+            )
+
+        all_cumulate = [accumulate_m1, accumulate_m2, accumulate_m11, accumulate_m12, accumulate_m22]
+
+        return all_cumulate, counter, accumulate_all_ms_func, reset_all, [get_cov11_func, get_cov22_func, get_cov12_func], [cov11, cov22, cov12]
+
+    def monitor_updates_variance(self):
+        shapes = [p.get_value().shape for p in self.params]
+
+
+        #Getting our update direction
+        vars = [self.updates[p] - p for p in self.params]
+
+        all_cumulate, counter, accumulate_all_ms_func, reset_all, get_variable_variance_func, all_cov = self.tract_covariance(vars, None,
+                                                                                                                                   shapes, 1,
+                                                                                                                              [
+                                                                                                                                  self.inpv,
+                                                                                                                                  self.ep,
+                                                                                                                                  self.w,
+                                                                                                                                  self.n_mc,
+                                                                                                                                  self.n_iw,
+                                                                                                                                  self.lr])
+
+        self.all_cumulate = all_cumulate
+        self.counter_delta = counter
+        self.accumulate_all_delta_func = accumulate_all_ms_func
+        self.reset_delta = reset_all
+        self.get_delta_variance_func = get_variable_variance_func[0]
+
 
 
 
@@ -245,10 +333,10 @@ class VAE(object):
         ep = np.random.randn(n, n_mc, n_iw, ds[0]).astype(floatX)
         return self.accumulate_gradients_func(input, n_mc, n_iw, ep, w)
 
-    def accumulate_grads2(self, input, n_mc, n_iw, w):
+    def accumulate_delta_var(self, input, n_mc, n_iw, w, lr=lr_default):
         n = input.shape[0]
         ep = np.random.randn(n, n_mc, n_iw, ds[0]).astype(floatX)
-        return self.accumulate_grads2_func(input, n_mc, n_iw, ep, w)
+        return self.accumulate_all_delta_func(input, ep, w, n_mc, n_iw, lr)
 
     def get_data_var(self, n):
 
@@ -276,18 +364,19 @@ def train_model(model,epochs=10,bs=64,n_mc=1,n_iw=1,w=lambda t:1.):
 
         # For the variance:
 
-        model.reset()
-        for i in range(50000/bs):
-            x = train_x[i*bs:(i+1)*bs].reshape(bs,28*28)
-            model.accumulate_gradients(x, n_mc, n_iw, w(t))
+        #model.reset()
+        #for i in range(50000/bs):
+        #    x = train_x[i*bs:(i+1)*bs].reshape(bs,28*28)
+        #    model.accumulate_gradients(x, n_mc, n_iw, w(t))
 
+        model.reset_delta()
         for i in range(50000/bs):
             x = train_x[i:(i+1)].reshape(1,28*28)
-            model.accumulate_grads2(x, n_mc, n_iw, w(t))
+            model.accumulate_delta_var(x,n_mc,n_iw,w(t))
 
-        print "The average variance norm is:"
-        vars = model.get_data_var(train_x.shape[0])
-        vars_norm = [np.linalg.norm(g) for g in vars]
+        print "The average update norm is:"
+        vars = model.get_delta_variance_func()
+        vars_norm = np.linalg.norm(vars[-2])
         print np.mean(vars_norm)
 
 
@@ -375,9 +464,9 @@ if __name__ == '__main__':
 
     tests = [
         [10,20,1,1],
-        [10*50,50*20,1,1],
-        [10,20,50,1],
-        [10,20,1,50]
+        #[10*50,50*20,1,1],
+        #[10,20,50,1],
+        #[10,20,1,50]
     ]
 
     toplots = list()
@@ -404,44 +493,44 @@ if __name__ == '__main__':
 # norm of variance of gradient estimate
 #==============================================================================
 #    # variance of gradient
-#    model = VAE()
+#     model = VAE()
 #
-#    bs = 64
-#    for i in range(50000/bs):
+#     bs = 64
+#     for i in range(50000/bs):
 #        x = train_x[i*bs:(i+1)*bs].reshape(bs,28*28)
 #        loss = model.train(x,1,1,1.)
 #
 #
-#    f_grads = theano.function([model.inpv,model.ep,model.w,
+#     f_grads = theano.function([model.inpv,model.ep,model.w,
 #                               model.n_mc,model.n_iw],
 #                              model.grads)
-#    rms = lambda xx: (xx**2).mean()**0.5
-#    l2norm = lambda xx: (xx**2).sum()**0.5
+#     rms = lambda xx: (xx**2).mean()**0.5
+#     l2norm = lambda xx: (xx**2).sum()**0.5
 #
-#    x = train_x[0].reshape(1,28*28)
-#    n = x.shape[0]
+#     x = train_x[0].reshape(1,28*28)
+#     n = x.shape[0]
 #
 #
-#    norm_operator = rms
-#    def get_norm_var(mc,iw):
+#     norm_operator = rms
+#     def get_norm_var(mc,iw):
 #        norms = list()
 #        for i in range(100):
 #            ep = np.random.randn(n,mc,iw,ds[0]).astype(floatX)
 #            norms.append(f_grads(x,ep,1.,mc,iw))
 #        return [norm_operator(xx) for xx in np.array(norms).var(0)]
 #
-#    tests = [
+#     tests = [
 #        [1,1],
 #        [1,10],
 #        [1,100],
 #        [10,1],
 #        [100,1]
-#    ]
+#     ]
 #
-#    for (mc,iw) in tests:
+#     for (mc,iw) in tests:
 #        plt.plot(get_norm_var(mc,iw))
 #
 #
-#    plt.legend(['mc{}iw{}'.format(mc,iw) for (mc,iw) in tests],loc=1)
-#    plt.savefig('rms_var_params.jpg',format='jpeg')
-#
+#     plt.legend(['mc{}iw{}'.format(mc,iw) for (mc,iw) in tests],loc=1)
+#     plt.savefig('rms_var_params.jpg',format='jpeg')
+

vae_svrg.py
@@ -173,6 +173,7 @@ class VAE(object):
                                        self.grads_accumulate)
             ]
         else:
+            print "SVRG-RE"
             self.deltas = [
                g_c - (mini_cov/(all_var + 1e-6)).clip(-10, 10)*(g_p - (a / self.counter))
                for g_c, g_p, a, mini_cov, all_var in zip(self.grads_curr,
@@ -181,7 +182,6 @@ class VAE(object):
                                       self.all_grads_covariance[2], # the covariance between both
                                       self.grads_prev_variance)]
 
-        self.monitor_updates_variance()
 
 
 
@@ -189,6 +189,7 @@ class VAE(object):
                                            self.params_curr,
                                            learning_rate=self.lr)
 
+        self.monitor_updates_variance()
 
         self.reset_and_copy = theano.function(
             inputs=[], updates=(copy_to_prev_update +
@@ -238,7 +239,9 @@ class VAE(object):
     def monitor_updates_variance(self):
         shapes = [p.get_value().shape for p in self.params_curr]
 
-        vars = [y for x, y in self.updates]#[self.lr*g for g in self.deltas]
+
+        #Getting our update direction
+        vars = [y -p for (x, y), p in zip(self.updates.items(), self.params_curr)]#[self.lr*g for g in self.deltas]
         all_cumulate, counter, accumulate_all_ms_func, reset_all, get_variable_variance_func, all_cov = self.tract_covariance(vars, None,
                                                                                                                                    shapes, 1,
                                                                                                                                    [
@@ -492,6 +495,7 @@ def train_model(model,epochs=10,bs=64,n_mc=1,n_iw=1, cumulate_cov=False, w=lambd
         for x in data_generator():
             model.accumulate_gradients(x, n_mc, n_iw, w(t))
 
+        model.reset_delta()
         for x in data_generator(1):
 
             # variance of the updates
@@ -501,7 +505,6 @@ def train_model(model,epochs=10,bs=64,n_mc=1,n_iw=1, cumulate_cov=False, w=lambd
 
 
         print "The average update norm is:"
-        #vars = model.get_data_var(train_x.shape[0])
         vars = model.get_delta_variance_func()
         vars_norm = np.linalg.norm(vars[-2])
         print np.mean(vars_norm)
@@ -581,7 +584,7 @@ if __name__ == '__main__':
         ax = fig.add_subplot(2,2,i+1)
         ax.plot(all_updates_var[i])
         plt.title(tests[i])
-        plt.ylim((80,250))
+        #plt.ylim((80,250))
     plt.savefig('vae_svrg_var.jpg',format='jpeg')
     plt.savefig('vae_svrg_var.tiff',format='tiff')
 